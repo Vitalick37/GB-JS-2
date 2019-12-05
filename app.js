c5232d10cf9cf3478836436b1b9772b64cf56181
Vitalick37 <vit198837@mail.ru>
2019-12-05 12:58:44 +0300

    1 задание

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+const goods = [{
+        title: 'Shirt',
+        price: 150
+    },
+    {
+        title: 'Socks',
+        price: 150
+    },
+    {
+        title: 'Jacket',
+        price: 150
+    },
+    {
+        title: 'Shoes',
+        price: 150
+    },
+];
+
+const renderGoodsItem = (title = 'Товар', price = 'Цена') => `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+
+
+const renderGoodsList = (list) => {
+    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+    document.querySelector('.goods-list').innerHTML = goodsList.join(' ');
+};
+
+renderGoodsList(goods);
