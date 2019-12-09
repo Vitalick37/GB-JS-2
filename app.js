ed98b846672003d13692d01487555ce6d1958a65
Vitalick37 <vit198837@mail.ru>
2019-12-09 14:44:35 +0300

    пустые классы для корзины товаров и элемента корзины товаров.

--- app.js
+++ app.js
@@ -28,48 +28,96 @@
 
 // renderGoodsList(goods);
 
-class GoodsItem {
-constructor (title, price) {
-    this.title = title;
-    this.price = price;
-}
-render () {
-   return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
-}
-};
 
+// 
+// 
+// Товар на странице
+
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `<div class="goods-item"><img src="img/photo_widjet_3.png" alt="foto"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
+};
+// 
+// 
+// Список товаров на странице
 class GoodsList {
     constructor() {
         this.goods = [];
     }
-fetchGoods() {
-    this.goods = [{
-        title: 'Shirt',
-        price: 150
-    },
-    {
-        title: 'Socks',
-        price: 150
-    },
-    {
-        title: 'Jacket',
-        price: 150
-    },
-    {
-        title: 'Shoes',
-        price: 150
-    },
-];
-}
-render() {
-    let listHtml = '';
-    this.goods.forEach(good => {
-        let goodsItem = new GoodsItem(good.title, good.price);
-        listHtml += goodsItem.render();
-    });
-    document.querySelector('.goods-list').innerHTML = listHtml;
-}
+    fetchGoods() {
+        this.goods = [{
+                title: 'Shirt',
+                price: 150
+            },
+            {
+                title: 'Socks',
+                price: 150
+            },
+            {
+                title: 'Jacket',
+                price: 150
+            },
+            {
+                title: 'Shoes',
+                price: 150
+            },
+        ];
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            let goodsItem = new GoodsItem(good.title, good.price);
+            listHtml += goodsItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+    sumGoods() {
+        let sum = 0;
+        this.goods.forEach(good => {
+            sum += good.price;
+        });
+        console.log(sum);
+    }
 };
 let list = new GoodsList();
 list.fetchGoods();
 list.render();
+list.sumGoods();
+// 
+// 
+// Товар в корзине
+
+class CrateGoodsItem {
+    constructor(title, price, count) {
+        this.title = title;
+        this.price = price;
+        this.count = count;
+    }
+    render() {
+        return `<div class="crate-item"><h3>${this.title}</h3><p>${this.price}</p><p>${this.count}</p></div>`;
+    }
+};
+// 
+// 
+// Крзина товаров
+class CrateGoods {
+    constructor() {
+        this.goods = [];
+    }
+    addItemCrate(newItem) {
+        this.goods.push(newItem);
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            let goodsItem = new CrateGoodsItem(good.title, good.price, good.count);
+            listHtml += goodsItem.render();
+        });
+       
+    }
+};
