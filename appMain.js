320c9d0c1bef5daac7442949ab7ba53797260341
Vitalik <vit198837@mail.ru>
2019-12-23 19:02:56 +0300

    пример с урока

--- appMain.js
+++ appMain.js
@@ -1,4 +1,50 @@
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 const app = new Vue({
     el: '#app',
-    data:
+    data: {
+        goods: [],
+        filterGoods: [],
+        searchLine: ''
+        
+    },
+
+    computed: {
+       
+    },
+    methods: {
+        makeGETRequest(url) {
+            return new Promise((resolve, reject) => {
+                let xhr;
+                if (window.XMLHttpRequest) {
+                    xhr = new window.XMLHttpRequest();
+                } else {
+                    xhr = new window.ActiveXObject('Microsoft.XMLHTTP');
+                }
+
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState == 4 && xhr.status == 200) {
+                        let body = (JSON.parse(xhr.responseText));
+                        resolve(body);
+                    }
+                    xhr.onerror = function (err) {
+                        PromiseRejectionEvent(err);
+                    }
+
+                }
+                xhr.open('GET', url);
+                xhr.send();
+            });
+        }
+
+    },
+    async mounted() {
+        try {
+            this.goods = await this.makeGETRequest(`${API_URL}/catalogData.json`);
+            this.filterGoods = [... this.goods];
+        } catch (e) {
+            console.error(e);
+        }
+    }
+   
 });
